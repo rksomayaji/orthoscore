daa9fa7ba8736f5bb35a76f16055470dff4c5aea
rksomayaji <rksomayaji@gmail.com>
2016-09-17 03:33:40 +0530

    Added update option on About Activity

--- app/src/main/java/com/rksomayaji/work/orthoscores/MainActivity.java
+++ app/src/main/java/com/rksomayaji/work/orthoscores/MainActivity.java
@@ -1,8 +1,12 @@
 package com.rksomayaji.work.orthoscores;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,28 @@ public class MainActivity extends AppCompatActivity {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.add(R.id.fragment_container,womacFragment);
             ft.commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu_about:
+                Intent aboutIntent = new Intent(this,AboutActivity.class);
+                startActivity(aboutIntent);
+                return true;
+            case R.id.menu_settings:
+                return true;
+            default:
+                return true;
         }
     }
 }
